3d5c02e2c95726b30152c6505fbf9793545dd30b
MikiManor <mikimanor@gmail.com>
2017-01-17 17:19:12 +0200

    Update Car.cs

--- Ex03.GarageLogic/Car.cs
+++ Ex03.GarageLogic/Car.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace Ex03.GarageLogic
+{
+    public class Car : Veicle
+    {
+        public enum Color
+        {
+            Blue = 1;
+            White = 2;
+            Black = 3;
+            Silver = 4;
+        }
+        private readonly int k_NumOfDoors;
+        
+    }
+}
